ad6d3cad723a4b0af0a1c42188c5347551934396
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-09-30 12:00:38 +0100

    added slot

--- src/main/java/Slot.java
+++ src/main/java/Slot.java
@@ -1,2 +1,24 @@
 public class Slot {
+
+    private SlotType slotType;
+
+    public Slot(SlotType slotType) {
+        this.slotType = slotType;
+    }
+
+    public SlotType getSlotType() {
+        return slotType;
+    }
+
+    public int getPositionValueFromEnum(){
+        return this.slotType.getPosition();
+    }
+
+    public int getRowValueFromEnum(){
+        return this.slotType.getRow();
+    }
+
+    public int getColumnValueFromEnum(){
+        return this.slotType.getColumn();
+    }
 }
