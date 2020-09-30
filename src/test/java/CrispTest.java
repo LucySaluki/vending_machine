897db858acf65a29711d6552ba061ade1a8140a2
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-09-30 11:38:18 +0100

    Crisp tests added

--- src/test/java/CrispTest.java
+++ src/test/java/CrispTest.java
@@ -1,2 +1,30 @@
+import Products.Crisp;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CrispTest {
+
+    private Crisp crisp;
+
+    @Before
+    public void before(){
+        crisp = new Crisp("Salt & Vinegar", "Walkers", 1.00);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Salt & Vinegar", crisp.getName());
+    }
+
+    @Test
+    public void canGetBrand(){
+        assertEquals("Walkers", crisp.getBrand());
+    }
+
+    @Test
+    public void canGetPrice(){
+        assertEquals(1.00, crisp.getPrice(),0.01);
+    }
 }
